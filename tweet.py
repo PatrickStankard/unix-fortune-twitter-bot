a5050d78327ebe4b07415bd2dd2bc76a6959cd61
Patrick Stankard <me@patrickstankard.com>
2014-02-05 22:58:05 +0000

    don't repeat any of the last 200 fortunes

--- tweet.py
+++ tweet.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ConfigParser import ConfigParser
-from os import path
-from subprocess import Popen, PIPE
 from re import sub
-from tweepy import OAuthHandler, API
+from os import path
 from sys import exit
+from tweepy import OAuthHandler, API
+from subprocess import Popen, PIPE
+from ConfigParser import ConfigParser
 
-def get_api_client():
+def get_config():
     config = ConfigParser()
     ini = path.join(path.abspath(path.dirname(__file__)), 'config.ini')
 
     config.read(ini)
 
+    return config
+
+def get_api_client():
+    config = get_config()
+
     consumer_key = config.get('Consumer', 'key')
     consumer_secret = config.get('Consumer', 'secret')
-
     access_key = config.get('Access', 'key')
     access_secret = config.get('Access', 'secret')
 
@@ -25,7 +29,7 @@ def get_api_client():
 
     return API(auth)
 
-def get_fortune(retry_count=0):
+def get_fortune(retry_count=0, tweets=[]):
     cmd = Popen(['fortune'], stdout=PIPE, stderr=PIPE)
     out, err = cmd.communicate()
 
@@ -35,23 +39,29 @@ def get_fortune(retry_count=0):
         out = out.strip()
 
         if len(out) > 140:
-            return retry_fortune(retry_count)
+            return retry_fortune(retry_count, tweets)
+
+        for tweet in tweets:
+            if tweet.text == out:
+                return retry_fortune(retry_count, tweets)
 
         return out
     if err:
-        return retry_fortune(retry_count)
+        return retry_fortune(retry_count, tweets)
 
-def retry_fortune(retry_count=0):
+def retry_fortune(retry_count=0, tweets=[]):
     retry_count += 1
 
-    if retry_count > 10:
+    if retry_count > 225:
         exit(1)
 
-    return get_fortune(retry_count)
+    return get_fortune(retry_count, tweets)
 
 def tweet_fortune():
     api_client = get_api_client()
-    fortune = get_fortune()
+    tweets = api_client.user_timeline(count=200)
+
+    fortune = get_fortune(0, tweets)
 
     api_client.update_status(fortune)
 
